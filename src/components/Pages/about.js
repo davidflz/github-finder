300d48540c30b00806b8d87f86860993dfed226d
DESKTOP-BROJ6IS\Balelas <david.felizardo@outlook.pt>
2021-08-06 14:48:09 +0100

    Website deploy

---
+++ src/components/Pages/about.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export const about = () => {
+	return (
+		<Fragment>
+			<h1>About</h1>
+			<p>App to search Github users</p>
+			<p>version 1.0.0</p>
+		</Fragment>
+	);
+};
